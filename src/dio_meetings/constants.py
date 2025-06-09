a05579b49044a897fee9dadc25e5ec52bd26b5c9
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-06-09 10:47:55 +0500

    09.06.2025 refactor

--- src/dio_meetings/constants.py
+++ src/dio_meetings/constants.py
@@ -5,10 +5,11 @@ from pathlib import Path
 
 # Директория проекта:
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 # Переменные окружения:
 ENV_PATH = BASE_DIR / ".env"
 
-
+# Драйверы для работы с Postgres
 PG_DRIVER: Literal["asyncpg"] = "asyncpg"
 
 
